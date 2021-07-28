cf5113449e57c9be717e357cadf0365c29c59d1c
Carlos Tadeu Panato Junior <ctadeu@gmail.com>
2021-07-28 17:55:26 +0200

    update tap to use cosign 1.0.0 (#2)
    * enable ubuntu tests
    
    Signed-off-by: Carlos Panato <ctadeu@gmail.com>
    
    * update cosign to 1.0.0
    
    Signed-off-by: Carlos Panato <ctadeu@gmail.com>

--- Formula/cosign.rb
+++ Formula/cosign.rb
@@ -4,32 +4,32 @@
 class Cosign < Formula
   desc "Container Signing, Verification and Storage in an OCI registry"
   homepage "https://sigstore.dev/"
-  version "0.6.0"
+  version "1.0.0"
   license "Apache-2.0"
   head "https://github.com/sigstore/cosign.git"
   bottle :unneeded
 
-  uses_from_macos "pcsc-lite"
+  uses_from_macos "pcsc-lite" if OS.mac?
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign_darwin_amd64_#{version}_darwin_amd64"
-    sha256 "357041436df8638036c35a57664b3c0d5ee9087887e63782749534a8b198f208"
+    url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-darwin-amd64"
+    sha256 "76656d48fa57f1bebbeaac8fdf5d17f590cdb45074e4d4fd5457a48f8e5ee497"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign_darwin_arm64_#{version}_darwin_arm64"
-    sha256 "11f17e47bb0985b047bd7374dbfae6f07c25c808e8c236ff784481a1473aaac8"
+    url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-darwin-arm64"
+    sha256 "f9b331c84117403d54e0c4e887db3eee58fc5059427e2ed251709c948f724d6e"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign_linux_amd64_#{version}_linux_amd64"
-    sha256 "00add9839794d76a2863fd534677ff48686d796dea06ccef6b82422a5955b9bf"
+    url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-linux-amd64"
+    sha256 "e36a05ab402bfee5463ad4752d8dc2941204c7b01a9a9931f921e91d94ba2484"
   end
 
   def install
     cpu = Hardware::CPU.intel? ? "amd64" : "arm64"
     os = OS.mac? ? "darwin" : "linux"
-    bin.install "cosign_#{os}_#{cpu}_#{version}_#{os}_#{cpu}" => "cosign"
+    bin.install "cosign-#{os}-#{cpu}" => "cosign"
   end
 
   test do
