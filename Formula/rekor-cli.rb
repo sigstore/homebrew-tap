63cf865bd485b21c4d3ca34f21ead653d439fb0a
Carlos Tadeu Panato Junior <ctadeu@gmail.com>
2021-08-10 14:45:04 +0200

    add rekor-cli to homebrew tap (#3)
    Signed-off-by: Carlos Panato <ctadeu@gmail.com>

---
+++ Formula/rekor-cli.rb
@@ -0,0 +1,36 @@
+# typed: false
+# frozen_string_literal: true
+
+class RekorCli < Formula
+  desc "Secure Supply Chain - Transparency Log"
+  homepage "https://sigstore.dev/"
+  version "0.3.0"
+  license "Apache-2.0"
+  head "https://github.com/sigstore/rekor.git"
+  bottle :unneeded
+
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/sigstore/rekor/releases/download/v#{version}/rekor-cli-darwin-amd64"
+    sha256 "6c10407d50403b702003f2ab4842073716f7354bab319a60673f15fd6d96cc70"
+  end
+
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/sigstore/rekor/releases/download/v#{version}/rekor-cli-darwin-arm64"
+    sha256 "5209b7c5e67f3306971bade7cc94208e74a39f4cdc18e53718e219642c7120cb"
+  end
+
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/sigstore/rekor/releases/download/v#{version}/rekor-cli-linux-amd64"
+    sha256 "ce9a7c82f32194995888758cf107ef0cc52e0b8cdce73b4240658ee9e73783cb"
+  end
+
+  def install
+    cpu = Hardware::CPU.intel? ? "amd64" : "arm64"
+    os = OS.mac? ? "darwin" : "linux"
+    bin.install "rekor-cli-#{os}-#{cpu}" => "rekor-cli"
+  end
+
+  test do
+    system "#{bin}/rekor-cli", "version"
+  end
+end
