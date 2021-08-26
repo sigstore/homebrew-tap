04e91650db9b08bc389c55bd344545d2a32b411b
Carlos Panato <ctadeu@gmail.com>
2021-08-26 11:06:21 +0200

    cosign: update formula to use cosign release v1.1.0
    Signed-off-by: Carlos Panato <ctadeu@gmail.com>

--- Formula/cosign.rb
+++ Formula/cosign.rb
@@ -4,7 +4,7 @@
 class Cosign < Formula
   desc "Container Signing, Verification and Storage in an OCI registry"
   homepage "https://sigstore.dev/"
-  version "1.0.0"
+  version "1.1.0"
   license "Apache-2.0"
   head "https://github.com/sigstore/cosign.git"
   bottle :unneeded
@@ -13,17 +13,17 @@ class Cosign < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-darwin-amd64"
-    sha256 "76656d48fa57f1bebbeaac8fdf5d17f590cdb45074e4d4fd5457a48f8e5ee497"
+    sha256 "786f2e084746581ac09ff4052bfbf4a8ba595e1e4d1602240b7a20a12780fd0b"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-darwin-arm64"
-    sha256 "f9b331c84117403d54e0c4e887db3eee58fc5059427e2ed251709c948f724d6e"
+    sha256 "953b26dca1dc208360ae5ff8b783a966c24e0894b4f39ad3e09818d77aa73fd0"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-linux-amd64"
-    sha256 "e36a05ab402bfee5463ad4752d8dc2941204c7b01a9a9931f921e91d94ba2484"
+    sha256 "c0b66f6948361f7f2c8c569d82d9471f36dd8354cf43f6bba6e578b31944127b"
   end
 
   def install
