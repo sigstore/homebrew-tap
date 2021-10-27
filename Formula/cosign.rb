361c84904091f32d2bba5dcdedaf3b2c89a10c3c
Carlos Tadeu Panato Junior <ctadeu@gmail.com>
2021-10-27 18:05:41 +0200

    Update cosign to v1.2.1 (#6)
    Signed-off-by: Carlos Panato <ctadeu@gmail.com>

--- Formula/cosign.rb
+++ Formula/cosign.rb
@@ -4,7 +4,7 @@
 class Cosign < Formula
   desc "Container Signing, Verification and Storage in an OCI registry"
   homepage "https://github.com/sigstore/cosign"
-  version "1.2.0"
+  version "1.2.1"
   license "Apache-2.0"
   head "https://github.com/sigstore/cosign.git"
   bottle :unneeded
@@ -14,19 +14,19 @@ class Cosign < Formula
 
     if Hardware::CPU.intel?
       url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-darwin-amd64"
-      sha256 "974b33f61004e235db9c5f57edb105ae0c1ff28b0bb4193a399f6b3bc9ac66fb"
+      sha256 "94872d7882e2fc6b8aebe864101395d2769f31783399282545156ccc2c516cf4"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-darwin-arm64"
-      sha256 "c86e50ede69216d4d3f45523c3f9aef265192290a49ee92bc1ffc73de7bbb131"
+      sha256 "917e56e1966d31a98e455904d4996563061ab0ce845e541434bba796b1f5b027"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-linux-amd64"
-      sha256 "690e9d6220aa715b61e332a0efefb60f4f4889cb8c0d0195d28a406425d617f0"
+      sha256 "490cb1941aa317cd24a0bd9f2fe38932805dbaaba0ae89c12ec8138d15bdd8a0"
     end
   end
 
