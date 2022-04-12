aa73c7ecc88ed0078dab611719d30dcf965b1117
cpanato <ctadeu@gmail.com>
2022-04-12 14:27:42 +0200

    update sget formula to release v1.7.2
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- Formula/sget.rb
+++ Formula/sget.rb
@@ -4,29 +4,29 @@
 class Sget < Formula
   desc "Automatic verification of signatures with our binary transparency log, Rekor"
   homepage "https://sigstore.dev"
-  version "1.7.1"
+  version "1.7.2"
   license "Apache-2.0"
-  head "https://github.com/sigstore/cosign.git", tag: "v1.7.1"
+  head "https://github.com/sigstore/cosign.git", tag: "v1.7.2"
 
   if Hardware::CPU.intel?
     url "https://github.com/sigstore/cosign/releases/download/v#{version}/sget-darwin-amd64"
-    sha256 "e317aa8afff22f0bd759ff2a56c4269bc36e0db80bbd21071f444c51a9ffeb85"
+    sha256 "742c0b44a799e3a619164af8f3bd2cdd0b6309aa335bdcc9f061196239ac2efa"
   end
 
   if Hardware::CPU.arm?
     url "https://github.com/sigstore/cosign/releases/download/v#{version}/sget-darwin-arm64"
-    sha256 "28a21789e000351d58a98bb5fd1b2425e2258945aa9e05c7fb594debf44baef7"
+    sha256 "5e7134c68e3b3b78d6f55db63b57f200cd958ce4cf0fa0b60ddcce36e734933a"
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/sigstore/cosign/releases/download/v#{version}/sget-linux-amd64"
-      sha256 "2b2fd3c481eafe8f219d0dd8423883e87c73d9ec91a22fb898d7c63871d7261a"
+      sha256 "fd1fc2143baadab82ac5a2fa6df6eb7a18b28485f04a50c8422b900881ddfc58"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/sigstore/cosign/releases/download/v#{version}/sget-linux-arm64"
-      sha256 "597bd30e09d4656aea30828b271299f1d252ab000cc4b67df08ddabe9c8b7c89"
+      sha256 "9631a9faaa49ad36742b7b83fa213cf7e8c95c6164b46c487ec52d581503ab51"
     end
   end
 
