fc3f7ce474c1a9b6cdcc611583e2d2f45e8ec975
Carlos Tadeu Panato Junior <ctadeu@gmail.com>
2022-06-14 18:28:48 +0200

    update gitsign to release v0.1.1 (#40)
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- Formula/gitsign.rb
+++ Formula/gitsign.rb
@@ -4,31 +4,31 @@
 class Gitsign < Formula
   desc "Keyless Git signing using Sigstore"
   homepage "https://sigstore.dev"
-  version "0.1.0"
+  version "0.1.1"
   license "Apache-2.0"
-  head "https://github.com/sigstore/gitsign.git", tag: "v0.1.0"
+  head "https://github.com/sigstore/gitsign.git", tag: "v0.1.1"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/sigstore/gitsign/releases/download/v#{version}/gitsign_#{version}_darwin_amd64"
-      sha256 "e8c5bed9e0834a097f037667007346ab927601f935667da771031b77d6029fee"
+      sha256 "1d73033d6507010d9f5aaba3993d98f0a21553eaa43f2fe3d09d3f047c04d2d8"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/sigstore/gitsign/releases/download/v#{version}/gitsign_#{version}_darwin_arm64"
-      sha256 "0c8c066adef87cdbb2b938c20b961408887be78b6fd15e9a93cc073af436082b"
+      sha256 "4f840596f70c580cf4d2c86b926e96b84983f94b1cf2c967b2386ddce3a0f79c"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/sigstore/gitsign/releases/download/v#{version}/gitsign_#{version}_linux_amd64"
-      sha256 "797b2c24d515f0b72fdbd3b3c1d938b9302f5d4f392a206ceaee51d487d1d70c"
+      sha256 "79db25fcbc4d4473c82437063907a01570a055d2929438c6d70ced492a0049f7"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/sigstore/gitsign/releases/download/v#{version}/gitsign_#{version}_linux_arm64"
-      sha256 "c2c0ed27fd376a24ce5b9edefb67bb9a01551705b837d37dd34b19547cc97e46"
+      sha256 "8da0239392d1add437c7e5cd98a75c4030f922375b70812cbbdbbe02ea6371d6"
     end
   end
 
