1960a644cc4fc13aabe83074ae05a2218043c76c
cpanato <ctadeu@gmail.com>
2022-06-17 11:45:43 +0200

    update formula to use rekor release v0.8.1
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- Formula/rekor-cli.rb
+++ Formula/rekor-cli.rb
@@ -4,31 +4,31 @@
 class RekorCli < Formula
   desc "Secure Supply Chain - Transparency Log"
   homepage "https://sigstore.dev"
-  version "0.8.0"
+  version "0.8.1"
   license "Apache-2.0"
-  head "https://github.com/sigstore/rekor.git", tag: "v0.8.0"
+  head "https://github.com/sigstore/rekor.git", tag: "v0.8.1"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/sigstore/rekor/releases/download/v#{version}/rekor-cli-darwin-amd64"
-      sha256 "2fca4376f2759c921e7e66e3d0c09e63c08a25745dcb10d0cace40b7cf61c3de"
+      sha256 "fb79038cfcd74146f2be2d6cbf64f8552ca504e6cf5e62bfea7680220fedee5c"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/sigstore/rekor/releases/download/v#{version}/rekor-cli-darwin-arm64"
-      sha256 "979a58b188d30577e354f8bcdbd42b327917fccbbd938a67a4641f39edaf4b4b"
+      sha256 "a60fb8e802946c2991c1d11aec0e472d7b510a7a5c99b07b0d4f9831d0623684"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/sigstore/rekor/releases/download/v#{version}/rekor-cli-linux-amd64"
-      sha256 "464c05ab11289351eb86a9d5a27e8b6eb689746d12d37ac47747b1c994d44653"
+      sha256 "d285e5924bb884a9daf6d46fac2d70d5883012296cf283014e1771fd98646c4d"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/sigstore/rekor/releases/download/v#{version}/rekor-cli-linux-arm64"
-      sha256 "2f2ae35e6e9b8c0ef696538562eb5bc1a49cc8ed6187f31cfd884dba43ebce39"
+      sha256 "31bff3a4b4401dffea95b51c319937bb5b85a52f493e10ec88b16aed2f34e88a"
     end
   end
 
