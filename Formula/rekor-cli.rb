eff5bec2772ed1ba5406d66fd28c2ef014f456bd
Carlos Tadeu Panato Junior <ctadeu@gmail.com>
2022-08-19 14:06:54 +0200

    update rekor-cli to v0.11.0 (#58)
    Signed-off-by: cpanato <ctadeu@gmail.com>
    
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- Formula/rekor-cli.rb
+++ Formula/rekor-cli.rb
@@ -4,31 +4,31 @@
 class RekorCli < Formula
   desc "Secure Supply Chain - Transparency Log"
   homepage "https://sigstore.dev"
-  version "0.10.0"
+  version "0.11.0"
   license "Apache-2.0"
-  head "https://github.com/sigstore/rekor.git", tag: "v0.10.0"
+  head "https://github.com/sigstore/rekor.git", tag: "v0.11.0"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/sigstore/rekor/releases/download/v#{version}/rekor-cli-darwin-amd64"
-      sha256 "ceb3c8686eda63923572eb625743841a90aa7916eaf4c1d33a921adcab25f97f"
+      sha256 "13b798529e603ab0827ea0f27b026babcb785cd3d18f18bb5ddc586c90b733c5"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/sigstore/rekor/releases/download/v#{version}/rekor-cli-darwin-arm64"
-      sha256 "b9cf92c4054ad4f53e456a37eb06a45987b8f19285efce4d5f3e0c0d7d99e598"
+      sha256 "71cd2bbeceb3872ff3ca6a8628bb2f8b680831b6dc7dd1a8796c93fce00b50e5"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/sigstore/rekor/releases/download/v#{version}/rekor-cli-linux-amd64"
-      sha256 "8bd6c237a902a2be3632bce7f84eaf62bee5cb2702f78a2d8ff8b6f7e5264af8"
+      sha256 "76ea21c367b9af60fed6a0966135722971d619c2b66f6ad6bf338b006452f4a0"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/sigstore/rekor/releases/download/v#{version}/rekor-cli-linux-arm64"
-      sha256 "c790e551b1cde42579662b97d8634653afcee376ded3957cd072b157ffbf62c3"
+      sha256 "49a6db8c205155c971152267fbce5b8474fc01ca5ecabcb4142906aa025f40f2"
     end
   end
 
