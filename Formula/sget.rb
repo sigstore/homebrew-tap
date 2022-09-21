49c816d78d662dd3b011177ec712146d059f4760
Carlos Tadeu Panato Junior <ctadeu@gmail.com>
2022-09-21 16:22:14 +0200

    bump sget to v1.12.1 (#68)
    Signed-off-by: cpanato <ctadeu@gmail.com>
    
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- Formula/sget.rb
+++ Formula/sget.rb
@@ -4,29 +4,29 @@
 class Sget < Formula
   desc "Automatic verification of signatures with our binary transparency log, Rekor"
   homepage "https://sigstore.dev"
-  version "1.12.0"
+  version "1.12.1"
   license "Apache-2.0"
-  head "https://github.com/sigstore/cosign.git", tag: "v1.12.0"
+  head "https://github.com/sigstore/cosign.git", tag: "v1.12.1"
 
   if Hardware::CPU.intel?
     url "https://github.com/sigstore/cosign/releases/download/v#{version}/sget-darwin-amd64"
-    sha256 "910565867d4c35051a7afca22642f9ba55c179ec5e0bfd081fef21ae5cdd0d04"
+    sha256 "cab5eb6c179a2300494afa24d21d67c2993acfb0cfb9ea78ab263428ba499429"
   end
 
   if Hardware::CPU.arm?
     url "https://github.com/sigstore/cosign/releases/download/v#{version}/sget-darwin-arm64"
-    sha256 "78b589b95047beaed501cf9596b63d679387a141b7539b482717673276831a7c"
+    sha256 "a58c8655a3921b194d5f8db3c8409a0ecabb32b7ae32d894f195e388d429fdae"
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/sigstore/cosign/releases/download/v#{version}/sget-linux-amd64"
-      sha256 "db3ce6398c148a3e17f5bad493e5d3c682725d3d1b70953c53bd0c3dc5e2449f"
+      sha256 "59dfe5417a8c137c8404eeba84b6029069fd6ab0c461dea0dd7ee80a05360e33"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/sigstore/cosign/releases/download/v#{version}/sget-linux-arm64"
-      sha256 "d861c7a7f6c066118e2aee82fb52d978c62ed825b590df50eb9238ed287490d3"
+      sha256 "83af722ed9122463d5044687857a05e73c077608e61406c6358ec08703b52447"
     end
   end
 
