c355f65c8b12a0c02b34d766e20317020ae569e0
Carlos Tadeu Panato Junior <ctadeu@gmail.com>
2022-10-17 13:56:48 -0700

    bump cosign to v1.13.1 (#77)
    Signed-off-by: cpanato <ctadeu@gmail.com>
    
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- Formula/cosign.rb
+++ Formula/cosign.rb
@@ -4,33 +4,33 @@
 class Cosign < Formula
   desc "Container Signing, Verification and Storage in an OCI registry"
   homepage "https://sigstore.dev"
-  version "1.13.0"
+  version "1.13.1"
   license "Apache-2.0"
-  head "https://github.com/sigstore/cosign.git", tag: "v1.13.0"
+  head "https://github.com/sigstore/cosign.git", tag: "v1.13.1"
 
   on_macos do
     depends_on "pcsc-lite"
 
     if Hardware::CPU.intel?
       url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-darwin-amd64"
-      sha256 "87c4f0cbab222c235a446f8675bcee9b2798c53ed00301f4458259a65dbc69a8"
+      sha256 "1d164b8b1fcfef1e1870d809edbb9862afd5995cab63687a440b84cca5680ecf"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-darwin-arm64"
-      sha256 "8b95f851af2de4ddf7b0f5d8f283f87cbb4a872a4f3367aacdaba67e491048e6"
+      sha256 "02bef878916be048fd7dcf742105639f53706a59b5b03f4e4eaccc01d05bc7ab"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-linux-amd64"
-      sha256 "45dbb8dd7d9f77eb010c091dea391da71730df2512d9fb1b65cd40136aee579e"
+      sha256 "a50651a67b42714d6f1a66eb6773bf214dacae321f04323c0885f6a433051f95"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-linux-arm64"
-      sha256 "950e39dbe7e182603d5d59d9e2a76e804d5d49bb9fb1b577510cb0bc7fd5a6a9"
+      sha256 "a7a79a52c7747e2c21554cad4600e6c7130c0429017dd258f9c558d957fa9090"
     end
   end
 
