73d4b9ecfff493c90ae3e854e5045f7381f69d0b
Billy Lynch <billy@chainguard.dev>
2023-04-11 12:22:59 -0400

    gitsign v0.6.0
    Signed-off-by: Billy Lynch <billy@chainguard.dev>

--- Formula/gitsign.rb
+++ Formula/gitsign.rb
@@ -4,33 +4,33 @@
 class Gitsign < Formula
   desc "Keyless Git signing using Sigstore"
   homepage "https://sigstore.dev"
-  version "0.5.2"
+  version "0.6.0"
   license "Apache-2.0"
-  head "https://github.com/sigstore/gitsign.git", tag: "v0.5.2"
+  head "https://github.com/sigstore/gitsign.git", tag: "v0.6.0"
 
   depends_on "sigstore/tap/gitsign-credential-cache"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/sigstore/gitsign/releases/download/v#{version}/gitsign_#{version}_darwin_amd64"
-      sha256 "4807d0a9c385442a309eddf7d45e5a383ca06e589def58bdf8af387ffb4a4dee"
+      sha256 "d2abbfcbf16ea79a2764562c9580cb4de5905ea9e24af2dc28c7b0c55d396208"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/sigstore/gitsign/releases/download/v#{version}/gitsign_#{version}_darwin_arm64"
-      sha256 "80251fe4c7f37a515214c329c90d52212cf160d75c41341201042a7741a1144b"
+      sha256 "6bb37112bd731a236317c4247e48b6c851bb01b3558cc7a53346f3b52cee70c8"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/sigstore/gitsign/releases/download/v#{version}/gitsign_#{version}_linux_amd64"
-      sha256 "e0ead963a99b966e5bbb3410a79ab330a7c863ee45ecd7b6e9e2247a6702fb1a"
+      sha256 "f711759b8ec7fa258df8c313d64309f64e6836f061ea5712b1bc5068dcd873c3"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/sigstore/gitsign/releases/download/v#{version}/gitsign_#{version}_linux_arm64"
-      sha256 "43121f61da0431336cf6da9cb5a5b84da32ff9934f2012f96610c5486ff581aa"
+      sha256 "8214928dbeb568d29c44a1a9b4fc5724d932d85026fd9afc0b21a993d362ebb2"
     end
   end
 
