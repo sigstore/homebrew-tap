35e890e2bb6853a1a1f91a16ab62224f3c793616
Carlos Tadeu Panato Junior <ctadeu@gmail.com>
2023-06-24 11:53:49 +0200

    bump cosign to v2.1.0 (#106)
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- Formula/cosign.rb
+++ Formula/cosign.rb
@@ -4,9 +4,9 @@
 class Cosign < Formula
   desc "Container Signing, Verification and Storage in an OCI registry"
   homepage "https://sigstore.dev"
-  version "2.0.2"
+  version "2.1.0"
   license "Apache-2.0"
-  head "https://github.com/sigstore/cosign.git", tag: "v2.0.2"
+  head "https://github.com/sigstore/cosign.git", tag: "v2.1.0"
 
   option "with-linux-piv-pkcs11", "Download cosign linux binary with piv/pkcs11 enabled"
 
@@ -15,12 +15,12 @@ class Cosign < Formula
 
     if Hardware::CPU.intel?
       url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-darwin-amd64"
-      sha256 "0f51cbe19a315b919e87042f0485331821722ecb7fce22cc1b880ed4833fc8b0"
+      sha256 "7ba6cf7a02a203e1978464f09551164ccacb9aefcfef8d3ec73e67af46417a91"
     end
 
     if Hardware::CPU.arm?
       url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-darwin-arm64"
-      sha256 "55242a52ebca43dfb133d0fe26e11546bfa4571addd6852d782c119d74deade1"
+      sha256 "c8ddd323d6b714105e4b05b48beba6b4f57c552464aefd4691d018bff6b4c362"
     end
   end
 
@@ -28,20 +28,20 @@ class Cosign < Formula
     if Hardware::CPU.intel?
       if build.with? "linux-piv-pkcs11"
         url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-linux-pivkey-pkcs11key-amd64"
-        sha256 "4e97e455a48a0e0751a16b2f84a67b527348df39dd6ae23c96b57b765b318084"
+        sha256 "9730c93b9d64dc6293a5ed31950587376df901582c732f175c91383f763079ba"
       else
         url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-linux-amd64"
-        sha256 "dc641173cbda29ba48580cdde3f80f7a734f3b558a25e5950a4b19f522678c70"
+        sha256 "c4fef1a4c7e49ce2006493b9aa894b28be247987959698b97de771c129cce8ea"
       end
     end
 
     if Hardware::CPU.arm?
       if build.with? "linux-piv-pkcs11"
         url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-linux-pivkey-pkcs11key-arm64"
-        sha256 "1b4b7ad0d608eccb5aab463a9e83f95186132868aa4078d21dc9c6b88eeab73b"
+        sha256 "b9541b25c467de096da555ffcd2c8baf68b5270a8b4aec034b82d709533c0ff6"
       else
         url "https://github.com/sigstore/cosign/releases/download/v#{version}/cosign-linux-arm64"
-        sha256 "517e96f9d036c4b77db01132cacdbef21e4266e9ad3a93e67773c590ba54e26f"
+        sha256 "f795a6903daadf764a5092599bfe6945cedd7656bef37884a3049ac1a529266c"
       end
     end
   end
